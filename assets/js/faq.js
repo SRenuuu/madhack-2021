4abe66e42e48d374f6c9dfd6ff45afdb63f50e72
Nethmi Rodrigo <neth23rodrigo@gmail.com>
2021-05-31 19:14:42 +0530

    Added animations

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -1,12 +1,16 @@
-var acc = document.getElementsByClassName("faq-button");
+var coll = document.getElementsByClassName("collapsible");
 var i;
-for (i = 0; i < acc.length; i++) {
-	acc[i].addEventListener("click", function () {
-		var panel = this.nextElementSibling;
-		if (panel.style.display === "block") {
-			panel.style.display = "none";
+
+for (i = 0; i < coll.length; i++) {
+	coll[i].addEventListener("click", function () {
+		this.classList.toggle("active");
+		var content = this.nextElementSibling;
+		if (content.style.maxHeight) {
+			content.style.maxHeight = null;
+			content.classList.remove("py-2", "lg:py-4", "sm:py-2", "md:py-4", "mb-2");
 		} else {
-			panel.style.display = "block";
+			content.classList.add("py-2", "lg:py-4", "sm:py-2", "md:py-4", "mb-2");
+			content.style.maxHeight = content.scrollHeight + "px";
 		}
 	});
 }
